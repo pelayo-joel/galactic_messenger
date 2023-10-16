f3f30a7f16b001cf6b86cf86ac3738aafd2bce3e
DEV_LAPTOP-PEL\pelay <joel.pelayo@laplateforme.io>
2023-10-16 14:28:35 +0200

    [Add] Created server and client branches

--- src/main/java/edu/laplateforme/server/ServerClass.java
+++ src/main/java/edu/laplateforme/server/ServerClass.java
@@ -1,4 +1,23 @@
 package edu.laplateforme.server;
 
+import java.sql.*;
+
 public class ServerClass {
+    private Connection connection = null;
+    private PreparedStatement sqlStatement = null;
+    private ResultSet statementResult = null;
+
+
+
+    public ServerClass() {
+        String url = "jdbc:mysql://localhost:3000/galactic_messenger";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager.getConnection(url, "root", "sqlroot");
+            System.out.println("Connected: " + this.connection);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
