2da54656631af11329bb2d2ed4ed94cc258a265e
DEV_LAPTOP-PEL\pelay <joel.pelayo@laplateforme.io>
2023-10-17 13:25:07 +0200

    [Update] PROJECT STRUCTURE REWORKED

---
+++ server/src/main/java/galactic/server/Server.java
@@ -0,0 +1,11 @@
+package galactic.server;
+
+import galactic.server.server_modules.*;
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Server {
+    public static void main(String[] args) {
+        ServerClass test = new ServerClass(Integer.parseInt(args[0]));
+        System.out.println("bro");
+    }
+}
--- server/src/main/java/galactic/server/server_modules/ServerClass.java
+++ server/src/main/java/galactic/server/server_modules/ServerClass.java
@@ -1,6 +1,9 @@
-package edu.laplateforme.server;
+package galactic.server.server_modules;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ServerClass {
     private Connection connection = null;
@@ -9,12 +12,12 @@ public class ServerClass {
 
 
 
-    public ServerClass() {
+    public ServerClass(int port) {
         String url = "jdbc:mysql://localhost:3000/galactic_messenger";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection(url, "root", "sqlroot");
+            this.connection = DriverManager.getConnection(url, "root", "mysqlroot");
             System.out.println("Connected: " + this.connection);
         } catch (Exception e) {
             System.out.println(e);
