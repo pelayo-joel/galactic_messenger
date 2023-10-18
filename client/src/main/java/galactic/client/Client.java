c51a1844f7166c931066bcc06cf000c3a85e05a9
Enzo Mattio <enzo.mattio@laplateforme.io>
2023-10-18 16:01:40 +0200

    [Add] New Fonctionnality: send and receive message from server

--- client/src/main/java/galactic/client/Client.java
+++ client/src/main/java/galactic/client/Client.java
@@ -1,44 +1,53 @@
 package galactic.client;
 import java.io.*;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Client {
     private Socket socket;
-    private ObjectInputStream reader;
+    private BufferedReader reader;
     private ObjectOutputStream writer;
 
     public Client(String serverAddress, int port) {
         try {
+            System.out.println("Creating socket...");
             socket = new Socket(serverAddress, port);
-            reader = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Socket created.");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream())) ;
+            System.out.println("Reader initialized.");
             writer = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Streams initialized.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        Client client = new Client("10.10.6.78", 4269);
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Client start...");
+        Client client = new Client("10.10.6.78", 4269);
+        System.out.println("Client connected to server");
         boolean send = false;
-        while (true) {
-            if (!send) {
-                String message = scanner.nextLine();
-                client.send(message);
-                send = true;
-            }
-            client.receive();
-        }
+
+
+        String message = scanner.nextLine();
+        client.send(message);
+
+
+
 
     }
 
     public void send(String message) {
         String[] message_split = message.split(" ", -1);
-
+//        System.out.println(Arrays.toString(message_split));
 
         try{
+
             writer.writeObject(message_split);
+            writer.flush();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -48,9 +57,9 @@ public class Client {
     public void receive() {
 
         try {
-            String message = (String) reader.readObject();
+            String message = (String) reader.readLine();
             System.out.println(message);
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
