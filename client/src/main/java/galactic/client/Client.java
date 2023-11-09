d9192c6cfc04098df6e3d9675d78ee71833814e4
Enzo Mattio <enzo.mattio@laplateforme.io>
2023-11-09 12:07:38 +0100

    [ADD] Hash Password client-side; Better Comment; Remove UDP socket

--- client/src/main/java/galactic/client/Client.java
+++ client/src/main/java/galactic/client/Client.java
@@ -2,9 +2,13 @@ package galactic.client;
 
 import galactic.client.modules.miscellaneous.Colors;
 
-import java.awt.*;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import java.io.*;
+import java.net.DatagramSocket;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.*;
 import java.util.List;
 
@@ -18,9 +22,12 @@ public class Client {
     public Client(String serverAddress, String port) {
         user_demands = new ArrayList<>();
         try {
+            // Create a socket to connect to the server
             System.out.println("Creating socket...");
             socket = new Socket(serverAddress, Integer.parseInt(port));
             System.out.println("Socket created.");
+
+            // Initialize input and output streams
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             System.out.println("Reader initialized.");
             writer = new ObjectOutputStream(socket.getOutputStream());
@@ -29,20 +36,46 @@ public class Client {
             e.printStackTrace();
         }
     }
-
     public void sendMessage(String message) {
         try {
-            ;
-            List<String> messageSplit = List.of(message.split(" ", 3));
-            if (messageSplit.get(0).equals("/accept") || messageSplit.get(0).equals("/decline")) {
-                if(user_demands.contains(messageSplit.get(1))) {
-                    user_demands.remove(messageSplit.get(1));
-                }else {
-                    System.out.println("You don't have any demands from this user.");
-                    return;
+            // Split the input message into a list of words
+            List<String> userInputs = new ArrayList<>(Arrays.asList(message.split(" ")));
+
+            if (userInputs.get(0).equals("/accept") || userInputs.get(0).equals("/decline")) {
+                // Process requests for private chats
+                for (String user : user_demands) {
+                    if (userInputs.get(1).equals(user)) {
+                        userInputs.add(user);
+                        user_demands.remove(user);
+                        break;
+                    }
                 }
             }
-            writer.writeObject(messageSplit);
+            if (userInputs.get(0).equals("/register") || userInputs.get(0).equals("/login")) {
+                // Hashing the password
+                char[] chars = userInputs.get(2).toCharArray();
+                byte[] salt = new byte[16];
+                try {
+                    PBEKeySpec pbeKeySpec = new PBEKeySpec(chars, salt, 1000, 64 * 8);
+                    SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+                    byte[] hashedPassword = keyFactory.generateSecret(pbeKeySpec).getEncoded();
+
+                    // Convert the hashed password and salt to Base64
+                    String hashedPasswordBase64 = Base64.getEncoder().encodeToString(hashedPassword);
+                    String saltBase64 = Base64.getEncoder().encodeToString(salt);
+
+                    userInputs.set(2, hashedPasswordBase64); // Replace the password with the hashed password
+                    userInputs.add(saltBase64); // Add the salt to the userInputs list
+
+                    // Now you have the hashed password and salt in userInputs for further processing
+                } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+                    // Handle any exceptions here
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println(userInputs);
+            writer.writeObject(userInputs);
             writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -51,12 +84,15 @@ public class Client {
 
     public String receiveMessage() {
         try {
+            // Read a message from the server
             String message = reader.readLine();
             if (message.startsWith("/dprivate")) {
+                // Handle private chat requests
                 String userRequest = message.substring(message.lastIndexOf(" ") + 1);
                 user_demands.add(userRequest);
+                System.out.println(user_demands);
                 return "User " + userRequest + " wants to talk with you.";
-            }else {
+            } else {
                 return message;
             }
         } catch (IOException e) {
@@ -67,6 +103,7 @@ public class Client {
 
     public void close() {
         try {
+            // Close the socket
             socket.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -76,7 +113,7 @@ public class Client {
     public void startMessageReceiver() {
         Thread messageReceiverThread = new Thread(() -> {
             while (true) {
-                // sleep 1s
+                // Sleep for 1 second
 
                 String message = receiveMessage();
                 if (message != null) {
@@ -88,7 +125,6 @@ public class Client {
         messageReceiverThread.setDaemon(true);
         messageReceiverThread.start();
     }
-
 
     public static void main(String[] args) throws IOException {
         System.out.println("Client start...");
@@ -102,12 +138,13 @@ public class Client {
             System.out.print("Enter a message: ");
             String message = scanner.nextLine();
             client.sendMessage(message);
-            if(message.equals("/disconnect") || message.equals("/quit")) {
+            if (message.equals("/disconnect") || message.equals("/quit")) {
                 System.out.println("Disconnecting...");
                 client.close();
                 break;
             }
             if (message.equals("/help")) {
+                // Display help information
                 System.out.println(Colors.RED + "For 1-to-1 messages:\n" +
                         Colors.BLUE + "\n/private_chat [target user] :" + Colors.DEFAULT + " Cette commande permet de demarrer une conversation privee avec un autre utilisateur en specifiant leur nom.\n" +
                         Colors.BLUE + "\n/accept [target user] :" + Colors.DEFAULT + " Utilise pour accepter une invitation à une discussion privee de la part d'un utilisateur specifie.\n" +
@@ -124,9 +161,9 @@ public class Client {
                         Colors.BLUE + "\n/join_secure_group [name] :" + Colors.DEFAULT + " Cette commande permet de rejoindre un groupe de discussion securise en specifiant son nom.\n");
 
                 System.out.println(Colors.RED + "For files command:\n" +
-                        Colors.BLUE + "\n/upload [user/group] [filename] :" + Colors.DEFAULT + " Utilise pour telecharger un fichier vers un utilisateur ou un groupe specifie.\n" +
+                        Colors.BLUE + "\n/upload [path-file] :" + Colors.DEFAULT + " Utilise pour telecharger un fichier vers un utilisateur ou un groupe specifie.\n" +
                         Colors.BLUE + "\n/list_files :" + Colors.DEFAULT + " Cette commande affiche la liste des fichiers disponibles dans la messagerie.\n" +
-                        Colors.BLUE + "\n/download [user/group] [filename] :" + Colors.DEFAULT + " Vous permet de telecharger un fichier depuis un utilisateur ou un groupe specifie.\n");
+                        Colors.BLUE + "\n/download [filename] :" + Colors.DEFAULT + " Vous permet de telecharger un fichier depuis un utilisateur ou un groupe specifie.\n");
 
                 System.out.println(Colors.RED + "To print online users:\n" +
                         Colors.BLUE + "\n/online_users :" + Colors.DEFAULT + " Affiche la liste des utilisateurs en ligne.\n");
@@ -135,8 +172,6 @@ public class Client {
                         Colors.BLUE + "\n/disconnect :" + Colors.DEFAULT + " Utilise pour quitter completement le programme et se deconnecter.\n" +
                         Colors.BLUE + "\n/quit :" + Colors.DEFAULT + " Utilise pour quitter completement le programme et se deconnecter." + Colors.DEFAULT);
             }
-
-
         }
     }
 }
